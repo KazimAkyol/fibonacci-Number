7f637fe46677dd1c52887ff8c5351e2c66fd9352
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-09-26 14:09:46 +0200

    second commit

---
+++ app.js
@@ -0,0 +1,21 @@
+//! Fibonacci
+// 0, 1, 1, 2, 3, 5, 8...
+
+// let solFibo = 0;
+// let sagFibo = 1;
+// let strFibonacci = "0 1";
+// let n = +prompt("kac tane fibonacci sayisi istersiniz?");
+
+// for (let i = 2; i < n; i++) {
+//   yeniFibo = solFibo + sagFibo;
+
+//   strFibonacci += `${yeniFibo}`;
+
+//   solFibo = sagFibo;
+//   sagFibo = solFibo;
+// }
+
+// console.log(strFibonacci);
+
+
+//! 
