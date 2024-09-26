b0ca3b45e86f581775c86c2efbcab229c1515e76
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-09-26 17:14:06 +0200

    8.commit

--- app.js
+++ app.js
@@ -1,40 +1,28 @@
-//! Fibonacci
-// 0, 1, 1, 2, 3, 5, 8...
-
+// //!Fibonacci
+// //0, 1, 1, 2, 3, 5, 8
 // let solFibo = 0;
 // let sagFibo = 1;
 // let strFibonacci = "0 1";
 // let n = +prompt("kac tane fibonacci sayisi istersiniz?");
-
 // for (let i = 2; i < n; i++) {
-//   yeniFibo = solFibo + sagFibo;
-
-//   strFibonacci += `${yeniFibo}`;
-
-//   solFibo = sagFibo;
-//   sagFibo = solFibo;
+//     yeniFibo = solFibo + sagFibo;
+//     strFibonacci += ` ${yeniFibo}`
+//     solFibo = sagFibo;
+//     sagFibo = yeniFibo;
 // }
-
 // console.log(strFibonacci);
-
-//! GCD EBOB
-
-// let a = +prompt("Birinci sayiyi giriniz");
-// let b = +prompt("Ikinci sayiyi giriniz");
+// //!GCD EBOB
+// let a = +prompt("Birinci sayiyi girin");
+// let b = +prompt("ikinci sayiyi girin");
 // let ebob = 1;
-
-// for (let i = 1; i <= a && i <= b; i++) {
-//   if (a % i === 0 && b % i === 0) {
-//     ebob = i;
-//   }
+// for (let i = 1; i <=a && i<=b; i++) {
+//     if (a % i ===0 && b % i ===0) {
+//         ebob = i;
+//     }
 // }
-
-// console.log("girilen sayilarin ebob'u " + ebob);
-
-//!Armstrong Sayilari
-
-//? Elimizdeki sayiyi rakamlara ayirip, basamak sayisini bu rakamlara üs olarak veriyoruz ve bu sayilari topluyoruz. Toplama islemi sonucu yine ayni sayi cikiyorsa elimizdeki sayiya Armstrong Sayi deriz.
-
+// console.log("girilen sayilarin ebob u " + ebob);
+//!Armstrog sayilari
+// Elimizdeki sayiyi rakamlara ayirip, basamak sayisini bu rakamlara üs olarak veriyoruz ve bu sayilari topluyoruz. Toplama işlemi sonucu yine ayni sayi çikiyorsa işte elimizdeki sayiya Armstrong Sayi deriz.
 // . 153: 1**3+5**3+3**3 = 1 + 125 + 27 = 153
 // . 370: 3**3+7**3+0**3 = 27 + 343 + 0 = 370
 // . 371: 3**3+7**3+1**3 = 27 + 343 + 1 = 371
@@ -43,47 +31,79 @@
 // . 8208
 // . 9474
 // . 54748
-
-// function armstrongNumber(num) {
-//   let basamak = 0;
-//   basamakSayisi = 0;
-//   sum = 0;
-//   while (num != 0) {
-//     basamak = num % 10;
-//     num = Math.trunc(num / 10);
-//     console.log(basamak);
-//     basamakSayisi++;
-//     sum = sum + Math.pow(basamak, basamakSayisi);
-//   }
-//   return `${basamakSayisi} ${sum}`;
+// //kullanıcıdan bir sayi al
+// //let number = prompt("Bir sayi girin:");
+// //let sum = 0;
+// //let temp = number;
+// //basamak sayısısnı bul
+// let numOfDigits = number.length;
+// //sayının her bir basamagını kontrol et
+// while (temp > 0) {
+//   let digit = temp % 10; // son basamagı al
+//   sum += Math.pow(digit, numOfDigits); // basamagın basamak sayısı kadar kuvvetini al ve toplama ekle
+//   temp = Math.floor(temp / 10); //bir sonraki basamaga gecmek icin sayıyı kucult
 // }
-// console.log(armstrongNumber(153));
-
-console.log("*********** Armstrong ****************")
-
-function armstrongNumber(num) {
-    let number = num;
-    let basamak;
-    let basamakSayisi = num.toString().length;
-    let sum = 0;
-
-    while (num != 0) {
-        basamak = num % 10
-        sum += Math.pow(basamak, basamakSayisi)
-        num = Math.trunc(num / 10);
-    }
-    if (sum === number) {
-        return `${number} armstrong bir sayidir`
-    } else {
-       return `${number} armstrong degildir`
-    }
+// //toplam girilen sayıya esitse armstrong sayısıdır
+// if (sum == number) {
+//   console.log(`${number} bir Armstrong sayisidir.`);
+// } else {
+//   console.log(`${number} bir Armstrong sayisi degildir.`);
+// }
+// function reversNumber(num) {
+//     var reverse = 0
+//     while (num != 0) {
+//         reverse =reverse * 10
+//         reverse =reverse + (num%10)
+//         num = Math.trunc(num/10)
+//     }
+//     return reverse
+// }
+// console.log(reversNumber(123));
+console.log("*********** Armstromg ****************");
+// function armstrongNumber(num) {
+//     let number = num;
+//     let basamak;
+//     let basamakSayisi = num.toString().length;
+//     let sum = 0;
+//     while (num != 0) {
+//         basamak = num % 10
+//         sum += Math.pow(basamak, basamakSayisi)
+//         num = Math.trunc(num / 10);
+//     }
+//     if (sum === number) {
+//         return `${number} armstrong bir sayidir`
+//     } else {
+//        return `${number} armstrong degildir`
+//     }
+// }
+// console.log(armstrongNumber(354))
+function isArmstrongNumber(num) {
+  //Sayının orjınalını saklayalım 1453
+  let orjinalNumber = num;
+  let digitCount = 0;
+  let sum = 0;
+  //Basamak sayısınız bulmak ıçın
+  while (orjinalNumber !== 0) {
+    orjinalNumber = Math.floor(orjinalNumber / 10); //Sayıyı 10'a bölerek basamak sayısını hesaplıyoruz
+    digitCount++; //her bölmede sayıyı artırdık
+  }
+  // Orjınal sayıya gerı dönelım
+  orjinalNumber = num;
+  //sayının her bır basamagını alıp basamak sayısına göre üssünü alıp toplayalım
+  while (orjinalNumber > 0) {
+    let digit = orjinalNumber % 10; //son basamagı bulduk
+    sum += digit ** digitCount; //Basamağın dıgıtcont kadar üsüünü ekle
+    orjinalNumber = Math.floor(orjinalNumber / 10); //sayıyı bır basamak eksılt
+  }
+  //Sayı ıle üssü alınan basamakaların toplamını karşılaştıralım
+  if (sum === num) {
+    return `${num} armstrong bir sayidir`;
+  } else {
+    return `${num} armstrong degildir`;
+  }
 }
-console.log(armstrongNumber(153))
-
-//! sayinin her bir basamagini alip basamak sayisina göre üssünü alip toplayalim:
-
-while (orjinalNumber > 0) {
-    let digit = orjinalNumber % 10; //son basamagi bulduk
-    sum += digit ** digitCount; //basamagin digitCount kadar üssünü ekle
-    orjinalNumber = Math.floor(orjinalNumber/10)
-}
+console.log(isArmstrongNumber(153));
+let number1 = prompt("say");
+console.log(typeof(number1));
+let numOfDigits2 = number1.length;
+console.log(numOfDigits2);
