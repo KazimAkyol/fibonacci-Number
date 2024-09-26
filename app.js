d26ca50531b2c7332324ee160ba8be315957fb12
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-09-26 15:01:03 +0200

    third commit

--- app.js
+++ app.js
@@ -17,5 +17,20 @@
 
 // console.log(strFibonacci);
 
+//! GCD EBOB
 
-//! 
+// let a = +prompt("Birinci sayiyi giriniz");
+// let b = +prompt("Ikinci sayiyi giriniz");
+// let ebob = 1;
+
+// for (let i = 1; i <= a && i <= b; i++) {
+//   if (a % i === 0 && b % i === 0) {
+//     ebob = i;
+//   }
+// }
+
+// console.log("girilen sayilarin ebob'u " + ebob);
+
+
+//!Armstrog Sayilari
+
